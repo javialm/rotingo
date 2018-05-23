f5477186e8d9894220f4cc94db630a7096c6a47e
javialm <javialm8@gmail.com>
2018-05-23 19:14:32 +0200

    Añadida funcion de selección de modo

--- usbt_version/main/main.ino
+++ usbt_version/main/main.ino
@@ -5,26 +5,28 @@
 
 #define MAX_SENSORES    1 // Numero de sensores
 #define PING_INTERVAL 33
-#define PING_CORRIENTE 10 
-unsigned long pingTimer[MAX_SENSORES]; 
-unsigned int cm[MAX_SENSORES];         
-uint8_t currentSensor = 0;    
-  
+#define PING_CORRIENTE 10
+unsigned long pingTimer[MAX_SENSORES];
+unsigned int cm[MAX_SENSORES];
+uint8_t currentSensor = 0;
+
 NewPing sonar[MAX_SENSORES] = {   //Array
   NewPing(11, 12, 100) //Trigger, Echo, Distancia maxima.
 };
 
 /*###### Bluetooth ######*/
-
-int state = 0;
+char aux = 0;
+String state = "";
+boolean newline = false;
 int fast = 150;
+int mode = 0;
 /*###### Motores ######*/
 
 // Motor A
 const int ENA = 6; // Pin PWM
 const int IN1 = 7;
 const int IN2 = 4;
- 
+
 // Motor B
 const int ENB = 5; // Pin PWM
 const int IN3 = 2;
@@ -33,76 +35,109 @@ const int IN4 = 3;
 /*######################## SETUP ###############################*/
 
 void setup() {
-    Serial.begin(9600); // Default communication rate of the Bluetooth module
-    pinMode(13, OUTPUT);
-    pinMode(ENA, OUTPUT);
-    pinMode(ENB, OUTPUT);
-    pinMode(IN1, OUTPUT);
-    pinMode(IN2, OUTPUT);
-    pinMode(IN3, OUTPUT);
-    pinMode(IN4, OUTPUT);
-    //digitalWrite(ledPin, LOW);
-    /*###### HC-SR04 ######*/
-    pinMode(PING_CORRIENTE, OUTPUT);
-    digitalWrite(PING_CORRIENTE, HIGH);
-    pingTimer[0] = millis() + 75;          
-    for (uint8_t i = 1; i < MAX_SENSORES; i++) 
+  pinMode(13, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  //digitalWrite(ledPin, LOW);
+  Serial.begin(9600); // Default communication rate of the Bluetooth module
+
+  /*###### HC-SR04 ######*/
+  pinMode(PING_CORRIENTE, OUTPUT);
+  digitalWrite(PING_CORRIENTE, HIGH);
+  pingTimer[0] = millis() + 75;
+  for (uint8_t i = 1; i < MAX_SENSORES; i++)
     pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
-    forward(); 
+  forward();
 }
 
 /*######################### LOOP ##############################*/
 
-void loop() 
-{
- if(Serial.available() > 0){ // Checks whether data is comming from the serial port
-    state = Serial.read(); // Reads the data from the serial port 
+void loop() { /*main*/
+
+  /*=== BLUETOOTH RECIEVE SERIAL DATA ===*/
+  state = "";
+  while (Serial.available() > 0) { // Checks whether data is comming from the serial port
+    aux = Serial.read(); // Reads the data from the serial port
+
+    if (aux == '\r') {
+      continue;
     }
- /*###### HC-SR04 ######*/
- for (uint8_t i = 0; i < MAX_SENSORES; i++) 
-  {
-    if (millis() >= pingTimer[i]) 
-    {     
-      pingTimer[i] += PING_INTERVAL * MAX_SENSORES;  
-      if (i == 0 && currentSensor == MAX_SENSORES - 1) oneSensorCycle();
-      sonar[currentSensor].timer_stop();          
-      currentSensor = i;                          
-      cm[currentSensor] = 0;                      
-      sonar[currentSensor].ping_timer(echoCheck);
+    else if (aux == '\n') {
+      newline = true;
+      break;
+    }
+    else {
+      state = state + aux;
+    }
+
+  }
+
+  Serial.print(state);
+  if (newline) {
+    Serial.print(state);
+  }
+
+  /*=== BLUETOOTH MODE CONTROL ===*/
+  if (state == '0') {
+    mode = 0;
+  }
+  else if (state == '1') {
+    mode = 1;
+  }
+
+  if (mode == 1) {
+    /*###### HC-SR04 ######*/
+    for (uint8_t i = 0; i < MAX_SENSORES; i++)
+    {
+      if (millis() >= pingTimer[i])
+      {
+        pingTimer[i] += PING_INTERVAL * MAX_SENSORES;
+        if (i == 0 && currentSensor == MAX_SENSORES - 1) oneSensorCycle();
+        sonar[currentSensor].timer_stop();
+        currentSensor = i;
+        cm[currentSensor] = 0;
+        sonar[currentSensor].ping_timer(echoCheck);
+      }
     }
   }
-  
- if (state == 'X'){
-  Serial.println("AAAAA A");
- }
- 
- switch (state){
-  case 'A':
-    digitalWrite(13, LOW);
-    Serial.println("A");
-    staphPlz();
-    state = 0;
-    break;
-  case 'B':
-    forward();
-   // state = 0;
-    break;
-  case 'C':
-    goback();
-    state = 0;
-    break;
-  case 'D':
-    turnLeft();
-    state = 0;
-    break;
-  case 'E':
-    turnRight();
-    state = 0;
-    break;
- }
-}
+
+  if (mode == 0) { //bluetooth mode
+    /*=== BLUETOOTH SWITCH CASE ===*/
+    if (state == "A") {
+      Serial.println("ALANTE");
+      forward();
+    } else if (state == "B") {
+      Serial.println("IZQUIERDA");
+      turnLeft();
+    } else if (state == "C") {
+      Serial.println("DERECHA");
+      turnRight();
+    } else if (state == "D") {
+      Serial.println("ATRAS");
+      goback();
+    } else if (state == "AB" || state == "BA") {
+      Serial.println("ALANTE-IZQUIERDA");
+      forwardLeft();
+    } else if (state == "AC" || state == "CA") {
+      Serial.println("ALANTE-DERECHA");
+      forwardRight();
+    } else if (state == "DB" || state == "BD") {
+      Serial.println("ATRAS-IZQUIERDA");
+      gobackLeft();
+    } else if (state == "DC" || state == "CD") {
+      Serial.println("ATRAS-DERECHA");
+      gobackRight();
+    }
+  }
+
+}//end loop
 
 /*######################## FUNCTIONS ###############################*/
+
 /*###### HC-SR04 ######*/
 void echoCheck() 
 {
@@ -128,29 +163,45 @@ void oneSensorCycle() {
 }
 
 /*##### orders #######*/
-void forward(){
-  Serial.println("PAAALANTEEEEE!!");
+void forward() {
   goA();
   goB();
 }
 
-void goback(){
-  Serial.println("PATRAAAAS!!");
+void forwardLeft() {
+  goA();
+  goHalfB();
+}
+
+void forwardRight() {
+  goHalfA();
+  goB();
+}
+
+void goback() {
   backA();
   backB();
 }
 
-void turnLeft(){
-  Serial.println("ISQUIERDAAA!!");
+void gobackLeft() {
+  backA();
+  backHalfB();
+}
+
+void gobackRight() {
+  backHalfA();
+  backB();
+}
+
+void turnLeft() {
   goB();
 }
 
-void turnRight(){
-  Serial.println("DEREEEECHAA!!");
+void turnRight() {
   goA();
 }
 
-void staphPlz(){
+void staphPlz() {
   stopA();
   stopB();
 }
@@ -158,35 +209,59 @@ void staphPlz(){
 
 /*##### basic functions #####*/
 
-void goA(){
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  analogWrite(ENA, fast);
-}
-
-void goB(){
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  analogWrite(ENB, fast);
-}
-
-void backA(){
-  
+void goA() {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   analogWrite(ENA, fast);
 }
 
-void backB(){
+void goHalfA() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, fast / 2);
+}
+
+void goB() {
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
   analogWrite(ENB, fast);
 }
 
-void stopA() {
-    analogWrite(ENA, LOW);// Motor A desactivado
+void goHalfB() {
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, fast / 2);
 }
- 
+
+void backA() {
+
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(ENA, fast);
+}
+
+void backHalfA() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(ENA, fast / 2);
+}
+
+void backB() {
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENB, fast);
+}
+
+void backHalfB() {
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENB, fast / 2);
+}
+
+void stopA() {
+  analogWrite(ENA, LOW);// Motor A desactivado
+}
+
 void stopB() {
-    analogWrite(ENB, LOW);// Motor B desactivado
+  analogWrite(ENB, LOW);// Motor B desactivado
 }
